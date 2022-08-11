4cafe74fbc084ddfa1e7825b5eb138e254846f0a
abhishek <abhishek.yps@gmail.com>
2022-08-11 22:37:00 +0530

    queue implementation

---
+++ queue.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueu(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else{
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+    dequeue(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.size === 1) {
+            this.first = null;
+            this.last = null;
+        }else{
+            this.first = this.first.next;
+        }
+        this.size--;
+        return temp;
+    }
+}
