073e38af6bc0ed7884ced4dd472fe061018ea125
abhishek <abhishek.yps@gmail.com>
2022-08-13 18:05:26 +0530

    Breadth first search implementation

--- BST.js
+++ BST.js
@@ -55,4 +55,19 @@ class BinarySearchTree {
             if(!found) return false
             return current;
         }
+        BFS(){
+         var node = this.root, queue = [], data = [];
+            queue.push(node);
+            while(queue.length){
+                node = queue.shift();
+                data.push(node.value);
+                if(node.left) queue.push(node.left);
+                if(node.right) queue.push(node.right);
+            }
+            return data;
+        }
+    
     }
+
+
+
